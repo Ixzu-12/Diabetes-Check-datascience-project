9a422528cf3a72cf6f6cd7b191eba3175005d15a
Aarish khan <khan.aarish2346@gmail.com>
2025-12-06 00:28:40 +0530

    Refactor diabetes prediction script for clarity
    Refactored diabetes prediction script by uncommenting and organizing code for clarity, including data preprocessing, model training, hyperparameter tuning, feature selection, and user input for predictions.

--- main.py
+++ main.py
@@ -24,55 +24,52 @@ from sklearn.model_selection import train_test_split
 Xtrain,Xtest,ytrain,ytest=train_test_split(X,y,random_state=33,test_size=0.2)
 
 
-# rf=RandomForestClassifier(n_estimators=10)
-# rf.fit(Xtrain,ytrain)
-# prediction=rf.predict(Xtest)
-# from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
-# print(classification_report(ytest,prediction))
-# print(confusion_matrix(ytest,prediction))
-# print(accuracy_score(ytest,prediction))
+rf=RandomForestClassifier(n_estimators=10)
+rf.fit(Xtrain,ytrain)
+prediction=rf.predict(Xtest)
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
+print(classification_report(ytest,prediction))
+print(confusion_matrix(ytest,prediction))
+print(accuracy_score(ytest,prediction))
 
-# # manual hyperparametetuning 
-# model=RandomForestClassifier(n_estimators=300,criterion='entropy',min_samples_leaf=10,random_state=100,max_features='sqrt')
-# model.fit(Xtrain,ytrain)
-# prediction=model.predict(Xtest)
-# from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
-# print(classification_report(ytest,prediction))
-# print(confusion_matrix(ytest,prediction))
-# print(accuracy_score(ytest,prediction))
+# manual hyperparametetuning 
+model=RandomForestClassifier(n_estimators=300,criterion='entropy',min_samples_leaf=10,random_state=100,max_features='sqrt')
+model.fit(Xtrain,ytrain)
+prediction=model.predict(Xtest)
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
+print(classification_report(ytest,prediction))
+print(confusion_matrix(ytest,prediction))
+print(accuracy_score(ytest,prediction))
 
-# from sklearn.model_selection import GridSearchCV,KFold,RandomizedSearchCV
-# rf=RandomForestClassifier()
-# rv=RandomizedSearchCV()
-# criteration=['entropy,gini']
-# n_estimators=[int(x) for x in np.linspace(start=200,stop=2000,num=10)]
-# max_features=['auto','sqrt','log2',]
-# max_depth=[int(x) for x in np.linspace(10,1000,10)]
-# min_sample=[1,3,5,7,9,11]
-# min_sample_leaf=[1,2,4,6,8]
-# randomgrid={'n_estimators': n_estimators,
-#             'max_features':max_features,
-#             'max_depth':max_depth,
-#             'min_sample':min_sample,
-#             'min_sample_leaf':min_sample_leaf,
-#             'criteration':criteration
-# }
-# rv=RandomizedSearchCV(estimator=rf,param_distributions=randomgrid,n_iter=100,cv=3,verbose=2,random_state=100,n_jobs=-1)
-# rv.fit(Xtrain,ytrain)
-# print(rv.best_params_)
-# best_random_rid=rv.best_estimator_
+from sklearn.model_selection import GridSearchCV,KFold,RandomizedSearchCV
+rf=RandomForestClassifier()
+rv=RandomizedSearchCV()
+criteration=['entropy,gini']
+n_estimators=[int(x) for x in np.linspace(start=200,stop=2000,num=10)]
+max_features=['auto','sqrt','log2',]
+max_depth=[int(x) for x in np.linspace(10,1000,10)]
+min_sample=[1,3,5,7,9,11]
+min_sample_leaf=[1,2,4,6,8]
+randomgrid={'n_estimators': n_estimators,
+            'max_features':max_features,
+            'max_depth':max_depth,
+            'min_sample':min_sample,
+            'min_sample_leaf':min_sample_leaf,
+            'criteration':criteration
+}
+rv=RandomizedSearchCV(estimator=rf,param_distributions=randomgrid,n_iter=100,cv=3,verbose=2,random_state=100,n_jobs=-1)
+rv.fit(Xtrain,ytrain)
+print(rv.best_params_)
+best_random_rid=rv.best_estimator_
 
-# automated hyperparameter tuning
-# bayesian optimization 
+automated hyperparameter tuning
+bayesian optimization 
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 from hyperopt import hp, fmin, tpe, STATUS_OK, Trials
-# model=RandomForestClassifier()
-# model.fit(Xtrain,ytrain)
-# ypred=model.predict(Xtest)
 from sklearn.metrics import f1_score,fbeta_score,roc_auc_score,auc,precision_score,mean_absolute_error,root_mean_squared_error,r2_score,classification_report,accuracy_score
 from sklearn.model_selection import StratifiedKFold,cross_val_score
-'''
+
 space = {
     "criterion": hp.choice('criterion', ['entropy', 'gini']),
     "max_depth": hp.quniform("max_depth", 10, 1200, 10),
@@ -113,12 +110,12 @@ print(cross_val_score(rf,X,y,cv=kfold))
 rf.fit(Xtrain,ytrain)
 ypred=rf.predict(Xtest)
 
-# print(classification_report(ytest,ypred))
-# print(roc_auc_score(ytest,ypred))
-# print(accuracy_score(ytest,ypred))
-# print(fbeta_score(ytest,ypred,beta=2,average='weighted'))
+print(classification_report(ytest,ypred))
+print(roc_auc_score(ytest,ypred))
+print(accuracy_score(ytest,ypred))
+print(fbeta_score(ytest,ypred,beta=2,average='weighted'))
 
-# print(best)uncomment to see the hyperparamter tuning params'''
+print(best)
 
 '''feature selection'''
 # 1st method
@@ -132,19 +129,19 @@ a=pd.DataFrame(variabel.scores_,columns=['Score'])
 b=pd.DataFrame(X.columns)
 
 c=pd.concat([a,b],axis=1)
-# print(c.nlargest(8,'Score'))
+print(c.nlargest(8,'Score'))
 '''2nd method'''
 from sklearn.ensemble import ExtraTreesClassifier
 ex=ExtraTreesClassifier()
 ex.fit(X,y)
 rank=pd.Series(ex.feature_importances_,index=X.columns)
 
-# print(rank.nlargest(8))
-# information gain
+print(rank.nlargest(8))
+information gain
 from sklearn.feature_selection import mutual_info_classif
 mutual=mutual_info_classif(X,y)
 mutual_data=pd.Series(mutual,index=X.columns)
-# print(mutual_data.sort_values(ascending=False))
+print(mutual_data.sort_values(ascending=False))
 
 from sklearn.feature_selection import RFE
 from sklearn.ensemble import RandomForestClassifier
@@ -152,8 +149,8 @@ from sklearn.ensemble import RandomForestClassifier
 rfe=RFE(estimator=RandomForestClassifier(),n_features_to_select=4)
 rfe.fit(X,y)
 for i, col in enumerate(X.columns):
-    # print(f"{col} selected={rfe.support_[i]} rank={rfe.ranking_[i]}")
-    pass
+    print(f"{col} selected={rfe.support_[i]} rank={rfe.ranking_[i]}")
+    
 
 
 
@@ -165,7 +162,7 @@ for i, col in enumerate(X.columns):
 3 Bmi
 4 Age
 """
-# first lets balance the data set
+first lets balance the data set
 from imblearn.combine import SMOTETomek
 sm=SMOTETomek(sampling_strategy='auto')
 Xtrain_ns,ytrain_ns=sm.fit_resample(Xtrain,ytrain)
@@ -204,3 +201,4 @@ if prediction==1:
 else:
     print("You dont have diabetes")
     
+
